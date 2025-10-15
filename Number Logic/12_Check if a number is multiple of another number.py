6fbcfd2c7d353fb12731cf0dae5c8a553b83841a
Elakkiya S R <159804084+elakkiyasaravanan@users.noreply.github.com>
2025-10-15 10:13:19 +0530

    Create 12_Check if a number is multiple of another number.py

---
+++ Number Logic/12_Check if a number is multiple of another number.py
@@ -0,0 +1,7 @@
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+
+if A % B == 0:
+    print(f"{A} is a multiple of {B}.")
+else:
+    print(f"{A} is not a multiple of {B}.")
