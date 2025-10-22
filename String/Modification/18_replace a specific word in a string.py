5c4f19df1ce16b83f27d03ba9222ea96aafb590e
Elakkiya S R <159804084+elakkiyasaravanan@users.noreply.github.com>
2025-10-22 11:38:46 +0530

    Create 18_replace a specific word in a string.py

---
+++ String/Modification/18_replace a specific word in a string.py
@@ -0,0 +1,6 @@
+text = input("Enter a string: ")
+old_word = input("Enter the word to replace: ")
+new_word = input("Enter the new word: ")
+
+result = text.replace(old_word, new_word)
+print("After replacement:", result)
