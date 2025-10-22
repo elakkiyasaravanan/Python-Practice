e36018623017eb29f51977ee5f2285cd30c6be76
Elakkiya S R <159804084+elakkiyasaravanan@users.noreply.github.com>
2025-10-22 12:57:30 +0530

    Create 02_Take n numbers as input from the user and store in a list.py

---
+++ List/Basic List Handling/02_Take n numbers as input from the user and store in a list.py
@@ -0,0 +1,8 @@
+n = int(input("How many numbers ? "))
+numbers = []
+
+for i in range(n):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num)  
+
+print("Your list is:", numbers)
