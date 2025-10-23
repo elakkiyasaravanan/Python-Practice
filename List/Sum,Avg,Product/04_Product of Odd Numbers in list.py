0fbddcf14ef47ad42bbe1cb7cdaecf2f68b810e4
Elakkiya S R <159804084+elakkiyasaravanan@users.noreply.github.com>
2025-10-23 18:57:20 +0530

    Create 04_Product of Odd Numbers in list.py

---
+++ List/Sum,Avg,Product/04_Product of Odd Numbers in list.py
@@ -0,0 +1,11 @@
+n=int(input("Enter n: "))
+number=[]
+for i in range(n):
+      num=int(input(f"Enter value {i+1}: "))
+      number.append(num)
+pro=1
+for i in number:
+      if i%2!=0:
+          pro*=i
+      
+print("Product of odd number is: ",pro)
